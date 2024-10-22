891dee1ad7422ee65a072b68b1630d45c48a3b5e
Cindy <cindy.mendes@hotmail.fr>
2024-10-22 16:23:06 +0200

    modify some functions

--- time_manager/lib/time_manager_web/controllers/user_controller.ex
+++ time_manager/lib/time_manager_web/controllers/user_controller.ex
@@ -7,6 +7,7 @@ defmodule TimeManagerWeb.UserController do
   #   users = Accounts.list_users()
   #   render(conn, :index, users: users)
   # end
+
   def index(conn, _params) do
     users = Accounts.list_users()
     conn
@@ -31,6 +32,32 @@ defmodule TimeManagerWeb.UserController do
     render(conn, :show, user: user)
   end
 
+  def show_user(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    conn
+    |> put_status(:ok)
+    |> json(%{
+      data: %{
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        team_id: user.team_id,
+        inserted_at: user.inserted_at,
+        updated_at: user.updated_at
+      }
+    })
+  rescue
+    Ecto.NoResultsError ->
+      conn
+      |> put_status(:not_found)
+      |> json(%{
+        errors: %{
+          detail: "User not found"
+        }
+      })
+  end
+
   def show_user_by_email_and_username(conn, %{"username" => username, "email" => email}) do
     user = Accounts.get_user_by_email_and_username(username, email)
     render(conn, :show, user: user)
@@ -52,21 +79,21 @@ defmodule TimeManagerWeb.UserController do
     end
   end
 
-  # def update(conn, %{"id" => id, "user" => user_params}) do
-  #   user = Accounts.get_user!(id)
-  #   user_params = filter_user_params(user_params)
-
-  #   with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
-  #     render(conn, :show, user: user)
-  #   else
-  #     {:error, changeset} ->
-  #       conn
-  #       |> put_status(:unprocessable_entity)
-  #       |> render(TimeManagerWeb.ChangesetView, "error.json", changeset: changeset)
-  #   end
-  # end
-
   def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+    user_params = filter_user_params(user_params)
+
+    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+      render(conn, :show, user: user)
+    else
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(TimeManagerWeb.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  def update_user(conn, %{"id" => id, "user" => user_params}) do
     user = Accounts.get_user!(id)
     user_params = filter_user_params(user_params)
 
@@ -103,6 +130,46 @@ defmodule TimeManagerWeb.UserController do
     with {:ok, %User{}} <- Accounts.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
+  end
+
+  def delete_user(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    
+    case Accounts.delete_user(user) do
+      {:ok, deleted_user} ->
+        conn
+        |> put_status(:ok)
+        |> json(%{
+          data: %{
+            id: deleted_user.id,
+            username: deleted_user.username,
+            email: deleted_user.email,
+            role: deleted_user.role,
+            team_id: deleted_user.team_id,
+            inserted_at: deleted_user.inserted_at,
+            updated_at: deleted_user.updated_at,
+            message: "User successfully deleted"
+          }
+        })
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{
+          errors: %{
+            detail: format_changeset_errors(changeset)
+          }
+        })
+    end
+  rescue
+    Ecto.NoResultsError ->
+      conn
+      |> put_status(:not_found)
+      |> json(%{
+        errors: %{
+          detail: "User not found"
+        }
+      })
   end
 
   # Helper function to filter the incoming user params
