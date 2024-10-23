70144ec5019e58c44ace4cc8fe90f991e05c19a3
olivier063 <90100162+olivier063@users.noreply.github.com>
2024-10-23 10:20:15 +0200

    feat: clocks ok

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,7 +9,7 @@ const apiClient = axios.create({
 
 // Intercepteur pour ajouter le token JWT à chaque requête
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('auth_token');
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
